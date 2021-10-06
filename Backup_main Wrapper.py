ff50fdd8e08a90dcd89d782c8a5fa359bc04e58a
Lachspooklord <88657158+Lachspooklord@users.noreply.github.com>
2021-10-06 15:25:04 +1100

    Wrapper.py
    Created 6/10/21

---
+++ Backup_main Wrapper.py
@@ -0,0 +1,17 @@
+from
+ New_Code import take_bkp
+
+src_file_name
+ = "file1" 
+
+bkp_file_name
+ = None
+
+src_file_loc
+ = "data/"
+
+bkp_file_loc
+ = "data/backup/"
+
+backup
+ = take_bkp(src_file_name, bkp_file_name, src_file_loc, bkp_file_loc)
