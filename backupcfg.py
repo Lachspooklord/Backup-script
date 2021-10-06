08789b48410a4b3f19e081c8f470de2c7b5da902
SuniTAFE <44331033+sunitafe@users.noreply.github.com>
2021-10-06 16:16:42 +1100

    Create backupcfg.py

---
+++ backupcfg.py
@@ -0,0 +1,6 @@
+logfile = '/home/ec2-user/environment/lhatfield-ictnwk409/logfile.txt'
+
+jobs = { 
+        "job1": { "src": '/home/ec2-user/environment/lhatfield-ictnwk409/data/file1', "dst": '/home/ec2-user/environment/lhatfield-ictnwk409/data/backup/'},
+        "job2": { "src": '/home/ec2-user/environment/lhatfield-ictnwk409/data/file2', "dst": '/home/ec2-user/environment/lhatfield-ictnwk409/data/backup/'}
+}
